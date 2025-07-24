220e1fa88503edc05d07a62ce28143e55cdaf342
Yasser <yassersuarez007@gmail.com>
2025-07-24 12:45:25 -0600

    Contruir Add Employee API REST

--- src/main/java/net/javaguides/ems/dto/EmployeeDto.java
+++ src/main/java/net/javaguides/ems/dto/EmployeeDto.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class EmployeeDto {
     private Long id;
-    private String firstname;
-    private String lastname;
+    private String firstName;
+    private String lastName;
     private String email;
 }
